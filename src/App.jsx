c6aa2ce931480fe75397c1f20928aa956e97860d
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-09-21 02:24:17 +0100

    setup project structure, set the routes, created the navbar

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import Collection from "./pages/collection.jsx";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
+import Product from "./pages/product.jsx";
+import Cart from "./pages/cart.jsx";
+import PlaceOrder from "./pages/placeOrder.jsx";
+import Orders from "./pages/orders.jsx";
+import NavBar from "./components/navBar.jsx";
+
+const App = () => {
+    return (
+        <div className={'px-4 sm:px-[5vw] md:px-[7vw] Ig:px-[7vw] lg:px-[9vw]'}>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/collection" element={<Collection/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/contact" element={<Contact/>} />
+                <Route path="/product/:producId" element={<Product/>} />
+                <Route path="cart" element={<Cart/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/place-order" element={<PlaceOrder/>} />
+                <Route path="/orders" element={<Orders/>} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/placeOrder.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PlaceOrder(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default PlaceOrder;
