bc2544ed8e0f3f9be14ddd460d111cadcf8ba756
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-09-23 14:35:34 +0100

    added: bestseller component to display bestselling products, ourPolicy component to display all policies, and newsLetterBox component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from "../components/hero.jsx";
 import LatestCollection from "../components/latestCollection.jsx";
+import BestSeller from "../components/bestSeller.jsx";
+import OurPolicy from "../components/ourPolicy.jsx";
 
 function Home() {
     return (
         <div>
             <Hero />
             <LatestCollection />
+            <BestSeller />
+            <OurPolicy />
         </div>
     );
 }
