18b295b1400988fea5fa26af301d8b3c05260cc0
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-09-25 15:35:59 +0100

    still improving on the product page

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,24 +1,28 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {ShopContext} from "../context/shopContext.jsx";
+import {assets} from "../assets/frontend_assets/assets.js";
 
 function Product() {
 
     const {productId} = useParams();
-    const {products} = useContext(ShopContext)
+    const {products, currency} = useContext(ShopContext)
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('');
+    const [size, setSize] = useState('');
 
     const fetchProductData = async () => {
         products.map((item) => {
-            if (item.id === productId) {
+            if (item._id === productId) {
                 setProductData(item);
                 // console.log(item)
-                setImage(item.image[ 0]);
+                setImage(item.image[0]);
                 return null;
             }
         })
     }
+
+    console.log(productData)
 
     useEffect(() => {
         fetchProductData();
@@ -31,10 +35,65 @@ function Product() {
                     <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
                         {
                             productData.image.map((item, index) => (
-                                <img src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt=""/>
+                                <img onClick={()=>setImage(item)} src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt=""/>
                             ))
                         }
                     </div>
+                    <div className="w-full sm:w-[80%]">
+                        <img className="w-full h-auto" src={image} alt=""/>
+                    </div>
+                </div>
+
+                <div className="flex-1">
+                    <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+                    <div className="flex items-center gap-1 mt-2">
+                        <img src={assets.star_icon} alt="" className="w-3 5"/>
+                        <img src={assets.star_icon} alt="" className="w-3 5"/>
+                        <img src={assets.star_icon} alt="" className="w-3 5"/>
+                        <img src={assets.star_icon} alt="" className="w-3 5"/>
+                        <img src={assets.star_dull_icon} alt="" className="w-3 5"/>
+                        <p className="pl-2">(122)</p>
+                    </div>
+                    <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
+                    <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+                    <div className="flex flex-col gap-4 my-8">
+                        <p>Select Size</p>
+                        <div className="flex gap-2">
+                            {
+                                productData.sizes.map((item, index)=> (
+                                    <button onClick={() => setSize(item)}
+                                            className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+                    <hr className="mt-8 sm:w-4/5"/>
+                    <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+                        <p>!00% Original Product.</p>
+                        <p>Cash on delivery available on this product.</p>
+                        <p>Easy return and exchange policy within 7 days.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-20">
+                <div className="flex">
+                    <b className="border px-5 py-3 text-sm">Description</b>
+                    <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+                </div>
+                
+                <div className="flex flex-col gap-4 border px-6 text-sm text-gray-500">
+                    <p>
+                        is an online platform that enables businesses to sell products or services, allowing customers
+                        to browse, make purchases, and complete transactions electronically. through a wide range of
+                        offerings, add items to their cart, and complete transactions online.
+                    </p>
+                    <p>
+                        Gone are the days of limited hours and crowded stores. The way we shop and do business has
+                        significantly shifted in recent years. Customers can shop anytime, anywhere, from the comfort
+                        of their homes or on the go over the internet, and E-commerce makes it happen.
+                    </p>
                 </div>
             </div>
         </div>
