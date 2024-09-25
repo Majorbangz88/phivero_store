56f65c3a66de02f6eedbef49ab35274222c70acc
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-09-25 11:13:15 +0100

    working on displaying each product details when clicked

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -7,7 +7,7 @@ function ProductItem(props) {
     const { id, image, name, price } = props;
     const {currency} = useContext(ShopContext);
     return (
-        <Link className="text-gray-700 cursor-pointer" to={`/products/${id}`}>
+        <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
             <div className="overflow-hidden">
                 <img className="hover:scale-110 transition ease-in-out" src={image[0]} alt=""/>
             </div>
