4d8e58e1b162e68761ed84e4a74081e94afab548
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-10-02 17:39:12 +0100

    added the place-order page and working on my-orders page

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -1,6 +1,7 @@
 import {createContext, useEffect, useState} from "react";
 import {products} from "../assets/frontend_assets/assets.js";
 import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
         if (!size) {
@@ -77,7 +79,7 @@ const ShopContextProvider = (props) => {
         products, currency, deliveryFee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart, getCartCount, updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
 
     return (
